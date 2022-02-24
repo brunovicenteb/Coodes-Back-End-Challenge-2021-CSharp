361954ba70643548e78e29b11931aa52c5595716
Bruno Belchior <brunovicenteb@gmail.com>
2022-02-24 09:21:20 -0300

    Adicionando interface XICustomQueryable

---
+++ Domain/Queries/XArticleQuery.cs
@@ -0,0 +1,21 @@
+﻿using AspNetCore.IQueryable.Extensions.Filter;
+using AspNetCore.IQueryable.Extensions.Attributes;
+using Coodesh.Back.End.Challenge2021.CSharp.Toolkit.Interfaces;
+
+namespace Coodesh.Back.End.Challenge2021.CSharp.Domain.Queries
+{
+    public class XArticleQuery : XICustomQueryable
+    {
+        public bool Featured { get; set; }
+
+        [QueryOperator(Operator = WhereOperator.Contains)]
+        public string Title { get; set; }
+
+        [QueryOperator(Operator = WhereOperator.Contains)]
+        public string Summary { get; set; }
+
+        public int? Limit { get; set; } = 10;
+
+        public int? Offset { get; set; }
+    }
+}
---
+++ Toolkit/Interfaces/XICustomQueryable.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore.IQueryable.Extensions;
+using AspNetCore.IQueryable.Extensions.Pagination;
+
+namespace Coodesh.Back.End.Challenge2021.CSharp.Toolkit.Interfaces
+{
+    public interface XICustomQueryable : IQueryPaging
+    {
+    }
+}
